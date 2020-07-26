10b4258d86d7184a655a8429e47b3bb704b4929f
Leandro Prado <57591950+leandro998@users.noreply.github.com>
2020-07-26 15:46:51 -0700

    Union Find

---
+++ SwiftAlgorithmDataStructure/Graphs/UnionFind/UF.swift
@@ -0,0 +1,87 @@
+//
+//  UF.swift
+//  SwiftAlgorithmDataStructure
+//
+//  Created by Leandro Prado on 2020-07-26.
+//  Copyright © 2020 Leandro Prado. All rights reserved.
+//
+
+import Foundation
+
+/// The UF struct represents a union-find (disjoint sets) data structure
+/// It supports **union** and **find** operations, along with methods for
+/// determining whether two nodes are in the same component and the total
+/// number of components.
+/// This implementation uses weighted quick union (by size or rank) with
+/// full path compression.
+/// Initializing a data structure with **n** nodes takes linear time.
+/// Afterwards, **union**, **find**, and **connected** take logarithmic time
+/// (in the worst case) and **count** takes constant time.
+/// Moreover, the amortized time per **union**, **find**, and **connected** operation
+/// has inverse Ackermann complexity (which is practically < 5 for 2^(2^(2^(2^16))) - undefined number).
+public struct UF {
+    /// parent[i] = parent of i
+    private var parent: [Int]
+    /// size[i] = number of nodes in tree rooted at i
+    private var size: [Int]
+    /// number of components
+    private(set) var count: Int
+    
+    /// Initializes an empty union-find data structure with **n** elements
+    /// **0** through **n-1**.
+    /// Initially, each elements is in its own set.
+    /// - Parameter n: the number of elements
+    public init(_ n: Int) {
+        self.count = n
+        self.size = [Int](repeating: 1, count: n)
+        self.parent = [Int](repeating: 0, count: n)
+        for i in 0..<n {
+            self.parent[i] = i
+        }
+    }
+    
+    /// Returns the canonical element(root) of the set containing element `p`.
+    /// - Parameter p: an element
+    /// - Returns: the canonical element of the set containing `p`
+    public mutating func find(_ p: Int) -> Int {
+        //canonical element(root) = parent
+        //this function will return the parent of p
+        if (parent[p] == p) {
+            return p
+        }
+        
+        parent[p] = find(parent[p])
+        return parent[p]
+        
+    }
+    
+    /// Returns `true` if the two elements are in the same set.
+    /// - Parameters:
+    ///   - p: one elememt
+    ///   - q: the other element
+    /// - Returns: `true` if `p` and `q` are in the same set; `false` otherwise
+    public mutating func connected(_ p: Int, _ q: Int) -> Bool {
+        //if find(p) == find(q) then it returns true and both parents are the same
+        return find(p) == find(q)
+    }
+    
+    /// Merges the set containing element `p` with the set containing
+    /// element `q`
+    /// - Parameters:
+    ///   - p: one element
+    ///   - q: the other element
+    public mutating func union(_ p: inout Int, _ q: inout Int) {
+        p = find(p)
+        q = find(q)
+        //if the size is bigger then it should hold the smaller one
+        if (size[p] > size[q]) {
+            let temp = p
+            p = q
+            q = temp
+        }
+        
+        parent[p] = q
+        size[q] += size[p]
+    
+    }
+}
